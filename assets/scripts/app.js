cbd78a6f987fa522857b7a9ba0cadb22ab7c0c5f
BenLirio <benjlirio@gmail.com>
2019-12-14 17:05:23 -0500

    Remaking the files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,36 @@
 'use strict'
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-const invoiceEvents = require('./invoices/events')
-const userController = require('./controllers/UserController')
-
+import ModelBase from ''
 $(() => {
-  setTimeout(() => {
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const userController = require('./controllers/UserController')
+
     // console.log(userController.invoices.find(1).items())
-    console.log(JSON.parse(JSON.stringify(userController)))
-    console.log(userController)
+    // console.log(JSON.parse(JSON.stringify(userController)))
+    // console.log(userController)
     // console.log(userController.invoices.find(1).items())
     // console.log('the invoice', userController.invoices.find(1))
     // console.log('the items of the invoice', userController.invoices.all()[0].items)
     // console.log('all items', userController.invoices.all()[0].items.all())
-  }, 0)
-})
